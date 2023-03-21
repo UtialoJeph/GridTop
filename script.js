523c1d1da8d72536808f41b132dedb9453557ffc
UtialoJeph <108458940+UtialoJeph@users.noreply.github.com>
2023-03-21 08:07:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function requestjs(){
+    document.getElementById("request").style.display="block";
+    document.getElementById("respond").style.display="none";
+    document.getElementById("doneit").style.display="none";
+
+    
+}
+
+function respondjs(){
+    document.getElementById("respond").style.display="block";
+    document.getElementById("request").style.display="none";
+    document.getElementById("doneit").style.display="none";
+
+
+   
+}
+
+function donejs(){
+    document.getElementById("doneit").style.display="block";
+    document.getElementById("request").style.display="none";
+    document.getElementById("respond").style.display="none";
+    
+}
