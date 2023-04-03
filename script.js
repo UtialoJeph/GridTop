4eef4e4c43bc1dc379e5ca34769ab0ad84bc70d4
UtialoJeph <108458940+UtialoJeph@users.noreply.github.com>
2023-04-03 18:15:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 
-function requestjs(){
-    document.getElementById("request").style.display="block";
-    document.getElementById("respond").style.display="none";
-    document.getElementById("doneit").style.display="none";
+let slideIndex = 1;
+showSlides(slideIndex);
 
-    
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function respondjs(){
-    document.getElementById("respond").style.display="block";
-    document.getElementById("request").style.display="none";
-    document.getElementById("doneit").style.display="none";
-
-
-   
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function donejs(){
-    document.getElementById("doneit").style.display="block";
-    document.getElementById("request").style.display="none";
-    document.getElementById("respond").style.display="none";
-    
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
